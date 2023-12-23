ee94d2f4b45b069108d9d7f69b8deb5fc718ec76
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2023-12-22 17:01:41 -0800

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,3 +1,10 @@
+/*
+DESCRIPTION:Practical implementation of the minimax algorithm. This version of the 
+            logic uses an optimized approach of a semi-intelligent decision making
+            process for the first move of the agent (regardless of token). Only 3 levels
+            of the tree are required.
+*/
+
 #include "game.h"
 #include "AI.h"
 #include "board_node.h"
